588832e7d091f3a4666c79f68b0570a1c212dcb9
Kruthi Villivalam <krvillia@microsoft.com>
2017-09-02 17:50:09 +0530

    first commit

--- init_container.sh
+++ init_container.sh
@@ -1,6 +1,8 @@
 
 #!/bin/bash
 
+service ssh start
+
 touch /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
